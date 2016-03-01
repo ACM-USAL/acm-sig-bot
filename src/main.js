7cfca1bc65eb10d8088c648cab8861df5daef2a5
Emilio Cobos Álvarez <ecoal95@gmail.com>
2016-03-01 16:52:58 +0100

    Refactor the filesystem structure
    This is the first step for more complex integrations.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,5 @@
 const fs = require('fs');
 const winston = require('winston');
-const http = require('http');
-const cheerio = require('cheerio');
 const utils = require('./utils');
 const emoji = require('node-emoji').emoji;
 
@@ -9,8 +7,9 @@ var TelegramBot = require('node-telegram-bot-api');
 
 winston.add(winston.transports.File, { filename: 'bot.log' });
 
-const TOKEN = require('./token');
-const GROUPS = require('./groups');
+const TOKEN = require('../config/token');
+const GROUPS = require('../config/groups');
+const poll_acm_respuestas = require('./poll-acm-respuestas.js');
 
 /// To ignore promise errors
 const promise_error = function () { winston.warn('Promise error: ', [].slice.call(arguments)); };
@@ -25,13 +24,17 @@ const LIST_TEXT = (function () {
   }).join('\n').trim();
 } ());
 
-/// The welcome message to send, with the group info
+// NOTE: This uses relative paths from the root of the package.
+//
+// It's intended to be run as:
+//    node src/main.js
+// The welcome message to send, with the group info
 const WELCOME_MESSAGE = (function () {
   const template = fs.readFileSync('msg/welcome.txt', { encoding: 'UTF-8' });
   return utils.render(template, { sig_list: LIST_TEXT });
 } ());
 
-/// Help message
+// Help message
 const HELP_MESSAGE = fs.readFileSync('msg/help.txt', { encoding: 'UTF-8' });
 
 /// New question message
@@ -146,124 +149,7 @@ function init(bot_user) {
           .catch(promise_error);
   });
 
-  init_polling_for_questions(bot, bot_user);
+  poll_acm_respuestas(bot, bot_user);
 }
 
 
-function init_polling_for_questions(bot, bot_user) {
-  const URL = 'http://usal.acm.org/preguntas/';
-
-  var last_question_id = null;
-  var poll_timer = null;
-  var poll;
-
-  const repoll = function () {
-    if ( poll_timer )
-      clearTimeout(poll_timer);
-
-    poll_timer = setTimeout(poll, POLL_INTERVAL_MS);
-  };
-
-  winston.info('Start polling to ' + URL);
-  poll = function() {
-    winston.info('HTTP GET request to ' + URL);
-    var request = http.request(URL);
-
-    /// Always schedule a re-poll when the response ends
-    request.on('response', function(response) {
-      response.on('end', repoll);
-    });
-
-    /// Also on error
-    request.on('error', function(err) {
-      winston.error('Request to ' + URL + ' failed: ' + err.message);
-      repoll();
-    });
-
-    request.on('response', function(response) {
-      var chunks = [];
-
-      if ( response.statusCode !== 200 ) {
-        winston.warn('Request to ' + URL + ' returned status ' + response.statusCode);
-        // The end event won't trigger until all data has been consumed
-        response.on('data', function() {});
-        return;
-      }
-
-      response.setEncoding('utf8');
-      response.on('data', function(chunk) {
-        chunks.push(chunk);
-      });
-
-      response.on('end', function() {
-        const $ = cheerio.load(chunks.join(''));
-
-
-        var newer_question_found = false;
-        var new_questions = [];
-        const previous_last_question_id = last_question_id;
-
-        $('.questions-list > article').each(function() {
-          const $this = $(this);
-          const id = parseInt($this.attr('id').replace('question-', ''), 10);
-
-          /// Don't send a message the first time
-          if ( last_question_id === null ) {
-            last_question_id = id;
-            winston.info('First question registered: ' + id);
-            return false;
-          }
-
-          /// Stop looping if we found the last previous question
-          if ( previous_last_question_id === id )
-            return false;
-
-          /// Now here we're sure the question is new
-
-          /// Update the last_question_id if this is a new question and
-          /// no newer question was found
-          if ( ! newer_question_found ) {
-            newer_question_found = true;
-            last_question_id = id;
-          }
-
-          winston.info('New question registered: ' + id);
-
-          const link = $this.find('.dwqa-title');
-          const question_url = link.attr('href');
-          const question_title = link.text();
-          const question_author = $this.find('.dwqa-author a').text();
-
-          new_questions.push({
-            id: id,
-            author_name: question_author,
-            question_title: question_title,
-            question_url: question_url,
-          });
-        });
-
-        if ( new_questions.length === 0 ) {
-          winston.info('No new questions this time');
-        } else {
-          winston.info(new_questions.length + ' new questions');
-
-          const questions = new_questions.map(function(question) {
-            return utils.render(NEW_QUESTION_MESSAGE, question);
-          });
-
-          const message = utils.render(NEW_QUESTIONS_MESSAGE, {
-            count: questions.length,
-            questions: questions.join('\n\n'),
-          });
-
-          bot.sendMessage(GROUPS.main_group_id, message)
-             .catch(promise_error);
-        }
-      });
-    });
-
-    request.end();
-  };
-
-  poll();
-}
